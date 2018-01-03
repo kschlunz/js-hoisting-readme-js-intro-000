868382adda9d764cbf7eac0abbabec795d0e6cd3
Kate Schlunz <schlunzk@gmail.com>
2018-01-03 15:10:04 +0000

    automatically backed up by learn

--- hoisting.js
+++ hoisting.js
@@ -11,9 +11,10 @@ function callMe() {
 
 function crazy() {
   // fix the code in here:
-  thisIsCrazy();
+
 
   var thisIsCrazy = function (){
+    thisIsCrazy();
     console.log("hey!!!")
   }
 }
