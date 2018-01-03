a7ed2d0a183562f32d48098e827b91ad57c5a1a2
Kate Schlunz <schlunzk@gmail.com>
2018-01-03 15:12:31 +0000

    automatically backed up by learn

--- hoisting.js
+++ hoisting.js
@@ -25,10 +25,11 @@ function sayMyName() {
   // fix the code in here:
   var name = "Cricky";
 
-  sayMy();
+
 
   function sayMy() {
     console.log(name);
     var name = "Kristin";
   }
+  sayMy();
 }
